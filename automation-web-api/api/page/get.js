e20dc20a0b2d458381c462a93c1f4dff425ab72e
sheilovani <sheilayovani@gmail.com>
2020-10-04 08:19:45 +0700

    move folder from automation to automation-web-api

---
+++ automation-web-api/api/page/get.js
@@ -0,0 +1,13 @@
+const supertest = require('supertest');
+const env = require('dotenv').config();
+
+const api = supertest(process.env.TEST_URL);
+
+const get = (userId, id, title, string) => api.get('')
+ .set('Content-Type', 'application/json')
+ .set('Accept', 'application/json')
+ .query(userId, id, title, string);
+
+module.exports = {
+   get,
+}
